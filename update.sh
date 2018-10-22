0b10847bf550b5835218b69428bdf8f0c0cca0d0
ihsgnef <shifeng@cs.umd.edu>
2018-10-22 15:27:18 -0400

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 cd ../codalab-cli/
-./scripts/competitiond.py -v ../SQuAD-explorer/config/codalab-v1.1.json ../SQuAD-explorer/out-v1.1.json
-./scripts/competitiond.py -v -l ../SQuAD-explorer/config/codalab-v2.0.json ../SQuAD-explorer/out-v2.0.json
-cd ../SQuAD-explorer/
+./scripts/competitiond.py -v -l ../SQuAD-explorer/config/qanta-config.json ../SQuAD-explorer/qanta-config.json
+cd ../pinafore-leaderboard/
 gulp
 gulp connect
