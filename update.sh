83aea41979f55cd7eda9a501f416f7b1523e9aff
sjtufs <sjtufs@gmail.com>
2018-10-24 12:23:05 -0400

    up

--- update.sh
+++ update.sh
@@ -1,5 +1,2 @@
 cd ../codalab-cli/
-./scripts/competitiond.py -v -l ../SQuAD-explorer/config/qanta-config.json ../SQuAD-explorer/qanta-config.json
-cd ../pinafore-leaderboard/
-gulp
-gulp connect
+./scripts/competitiond.py -v -l ../qanta-leaderboard/config/qanta.json qanta-out.json
