67aaf802bd9e3a2679d296abd384bf27fb4dd44a
ihsgnef <shifeng@cs.umd.edu>
2018-10-24 12:28:35 -0400

    update

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 cd ../codalab-cli/
-./scripts/competitiond.py -v -l ../qanta-leaderboard/config/qanta.json qanta-out.json
+./scripts/competitiond.py -v -l ../qanta-leaderboard/config/qanta.json ../qanta-leaderboard/out-qanta.json
